bb3b881a881722456ee76036db8efa1144668218
AlexandraMBratan <maria.bratan@student.upt.ro>
2022-05-06 23:37:50 +0300

    Adding Home Page for Admin

--- app/src/main/java/com/example/aticketapp/AdminHomePageActivity.java
+++ app/src/main/java/com/example/aticketapp/AdminHomePageActivity.java
@@ -1,14 +1,24 @@
 package com.example.aticketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.aticketapp.databinding.ActivityAdminHomePageBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminHomePageActivity extends AdminNavDrawerActivity {
 
     ActivityAdminHomePageBinding activityAdminHomePageBinding;
+    CardView cardConcertAdmin, cardFestivalAdmin,cardTeatruAdmin,cardSportAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,45 @@ public class AdminHomePageActivity extends AdminNavDrawerActivity {
         activityAdminHomePageBinding =  ActivityAdminHomePageBinding.inflate(getLayoutInflater());
         setContentView(activityAdminHomePageBinding.getRoot());
         setActionBarTitle("Home");
+
+        ImageSlider imageSlider = (ImageSlider) findViewById(R.id.slider);
+
+        cardConcertAdmin = (CardView) findViewById(R.id.cardConcerteAdmin);
+        cardFestivalAdmin = (CardView) findViewById(R.id.cardFestivalAdmin);
+        cardTeatruAdmin = (CardView) findViewById(R.id.cardTeatruAdmin);
+        cardSportAdmin = (CardView) findViewById(R.id.cardSportAdmin);
+
+        cardConcertAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminHomePageActivity.this, "Concerts Page", Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(this,ConcertPageAdmin.class));
+            }
+        });
+
+        cardFestivalAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminHomePageActivity.this, "Festivals Page", Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(this,FestivalPageAdmin.class));
+            }
+        });
+
+        cardTeatruAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminHomePageActivity.this, "Theatre Page", Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(this,TheatrePageAdmin.class));
+            }
+        });
+
+        cardSportAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminHomePageActivity.this, "Sports Page", Toast.LENGTH_LONG).show();
+                //startActivity(new Intent(this,SportPageAdmin.class));
+            }
+        });
+
     }
 }
